f749517774a20767abaaf7db07368f13c6ac89cb
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2023-06-07 05:55:01 +0600

    Basic Setup Ready

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Components/Layout/Routes/Routes.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>
+);
